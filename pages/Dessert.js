2c826556ae46345797b3c3ac9c6908d543d5fe74
Angela Hentschel <angela.neuron@gmail.com>
2022-10-04 17:43:33 +0200

    add image from external url into dessert page & allow use in config file

--- pages/Dessert.js
+++ pages/Dessert.js
@@ -1,10 +1,18 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 export default function Dessert() {
   return (
     <>
       <h2>Dessert recipes</h2>
       <p>some text...</p>
+      <Image
+        src="https://unsplash.com/photos/surQ2mkZNxw/download?"
+        alt="cake"
+        layout="responsive"
+        width={2400}
+        height={1599}
+      />
     </>
   );
 }
