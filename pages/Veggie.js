9bb9550fa639a90c3d7b5db484e1496b89e65350
Angela Hentschel <angela.neuron@gmail.com>
2022-10-04 17:08:27 +0200

    add pizza image to Veggie page

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,10 +1,18 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 export default function Veggie() {
   return (
     <>
       <h2>Veggie recipes</h2>
       <p>some text...</p>
+      <Image
+        src="/assets/pizza.jpg"
+        alt="pizza"
+        layout="responsive"
+        width={4256}
+        height={2832}
+      />
     </>
   );
 }
